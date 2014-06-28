bb0c40f7b83e27d13fe633f640d38bedfb2c1d8f
Otis <otis@hdtech.com.tw>
2014-06-29 03:22:05 +0800

    remove uuid

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -69,6 +69,6 @@ class NodesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def node_params
-      params.require(:node).permit(:name, :uuid)
+      params.require(:node).permit(:name)
     end
 end
