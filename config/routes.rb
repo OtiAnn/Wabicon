2d290f209200bf973311b99302f906ac5c7e856d
OtiAnn <anniebh30@gmail.com>
2014-06-28 21:37:08 +0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  get 'member/index'
-
-  get 'member/show'
-
-  get 'member/new'
-
-  get 'member/create'
-
-  get 'member/edit'
-
-  get 'member/update'
-
-  get 'member/destroy'
-
+  resources :member
   resources :users
   resources :nodes_users
   resources :nodes do
