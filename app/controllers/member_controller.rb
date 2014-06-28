52b22b8ee333caa2c69030ad8157c03cac6cd886
Otis <otis@hdtech.com.tw>
2014-06-28 20:20:53 +0800

    update member controller

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,14 +1,30 @@
 class MemberController < ApplicationController
   def index
+    @members = Event.find(params[:event_id]).users.all
+    respond_to do |format|
+      format.json {render json: @members, status: :ok }
+    end
   end
 
   def show
+
   end
 
   def new
   end
 
   def create
+    @event = Event.find(params[:event_id])
+    @member = @event.users.build(user_id: parms[:id])
+    respond_to do |format|
+      if @member.save
+        format.html
+        format.json {render json: @event.users, status: :ok }
+      else
+        format.html
+        format.json {render json: @member.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -18,5 +34,11 @@ class MemberController < ApplicationController
   end
 
   def destroy
+    @event = Event.find(params[:event_id])
+    @member = @event.users.delete(user_id: parms[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @event.users, status: :unprocessable_entity }
+    end
   end
 end
